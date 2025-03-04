4ed3845ed6b1f1e38505d7b6fcecafe99d6cc233
dehesan <3131338337@qq.com>
2025-03-04 15:26:54 +0800

    编辑员工业务代码开发

--- sky-server/src/main/java/com/sky/service/EmployeeService.java
+++ sky-server/src/main/java/com/sky/service/EmployeeService.java
@@ -20,4 +20,8 @@ public interface EmployeeService {
     PageResult pageQuery(EmployeePageQueryDTO employeePageQueryDTO);
 
     void startOrStop(Integer status, Long id);
+
+    Employee getid(Long id);
+
+    void update(EmployeeDTO employeeDTO);
 }
